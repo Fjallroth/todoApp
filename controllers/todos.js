aef462d1a81a3aaddf1f01e20fdfca5c54477446
Fjallroth <christer.fjallroth@gmail.com>
2022-11-10 13:39:03 +0100

    increased the todo Inputs

--- controllers/todos.js
+++ controllers/todos.js
@@ -4,16 +4,17 @@ module.exports = {
     getTodos: async (req,res)=>{
         console.log(req.user)
         try{
-            const todoItems = await Todo.find({userId:req.user.id})
-            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+            const todoItems = await Todo.find({userId:req.user.id}) //finds all Todos in the database tied to that user
+            const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false}) //counts the items that have completed value as false
+            res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user}) //renders todoItems in todos.ejs
         }catch(err){
             console.log(err)
         }
     },
     createTodo: async (req, res)=>{
         try{
-            await Todo.create({todo: req.body.todoItem, completed: false, userId: req.user.id})
+        //edited Todo.create to include new inputs
+            await Todo.create({todo: req.body.todoItem, todoDeadline: req.body.deadline, todoPriority: req.body.highPriority, completed: false, userId: req.user.id})
             console.log('Todo has been added!')
             res.redirect('/todos')
         }catch(err){
--- models/Todo.js
+++ models/Todo.js
@@ -1,3 +1,4 @@
+const { Long } = require('mongodb')
 const mongoose = require('mongoose')
 
 const TodoSchema = new mongoose.Schema({
@@ -8,6 +9,29 @@ const TodoSchema = new mongoose.Schema({
   completed: {
     type: Boolean,
     required: true,
+  },
+  //added added by to schema, this will be based on the userId. Can I just use user ID instead of having a separate field?
+  addedBy: {
+    type: String,
+    required: true
+  },
+  //added date added to the schema, this will be based on the time it was added
+  dateAdded: {
+    type: Date,
+    required: true
+  },
+  //date completed based on the update request time. The idea is to use this to hide the completed items from display after a certain period
+  dateCompleted: {
+    type: Date,
+    required: true
+  },
+  priority: {
+    type: Number,
+    required: false
+  },
+  deadLine: {
+    type: Date,
+    required: false
   },
   userId: {
     type: String,
