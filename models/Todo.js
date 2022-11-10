9d1b53e7897d78883a2a64fe174330fac35f7849
Fjallroth <christer.fjallroth@gmail.com>
2022-11-10 14:02:28 +0100

    added new fields to schema and input form

--- models/Todo.js
+++ models/Todo.js
@@ -13,30 +13,20 @@ const TodoSchema = new mongoose.Schema({
   //added added by to schema, this will be based on the userId. Can I just use user ID instead of having a separate field?
   addedBy: {
     type: String,
-    required: true
-  },
-  //added date added to the schema, this will be based on the time it was added
-  dateAdded: {
-    type: Date,
-    required: true
-  },
-  //date completed based on the update request time. The idea is to use this to hide the completed items from display after a certain period
-  dateCompleted: {
-    type: Date,
-    required: true
-  },
-  priority: {
-    type: Number,
     required: false
   },
-  deadLine: {
+  todoPriority: {
+    type: String,
+    required: false
+  },
+  todoDeadline: {
     type: Date,
     required: false
   },
   userId: {
     type: String,
     required: true
-  }
-})
+  },
+}, { timestamps: true })
 
 module.exports = mongoose.model('Todo', TodoSchema)
